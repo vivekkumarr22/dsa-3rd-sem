e3b55c97ac3eb439c7a4d47db3e9525e15336dcd
Vivek kumar <vivek.kumar.cse.2025@miet.ac.in>
2026-09-23 09:42:49 +0530

    Implement array deletion in C program

---
+++ 01_Array/array_deletion.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, pos, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter position to delete: ");
+    scanf("%d", &pos);
+
+    for(i = pos; i < n - 1; i++)
+        a[i] = a[i + 1];
+
+    n--;
+
+    printf("Array after deletion:\n");
+    for(i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
