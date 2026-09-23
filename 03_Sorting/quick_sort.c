a49e7ed4b23e85a1d6f245ea275cfa46b6a26e61
Vivek kumar <vivek.kumar.cse.2025@miet.ac.in>
2026-09-23 10:36:03 +0530

    Add quick sort implementation in quick_sort.c
    Implement quick sort algorithm to sort an array.

---
+++ 03_Sorting/quick_sort.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int partition(int a[], int low, int high) {
+    int pivot = a[high];
+    int i = low - 1;
+    int j, temp;
+
+    for(j = low; j < high; j++) {
+        if(a[j] < pivot) {
+            i++;
+
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+
+    temp = a[i + 1];
+    a[i + 1] = a[high];
+    a[high] = temp;
+
+    return i + 1;
+}
+
+void quickSort(int a[], int low, int high) {
+    if(low < high) {
+        int p = partition(a, low, high);
+
+        quickSort(a, low, p - 1);
+        quickSort(a, p + 1, high);
+    }
+}
+
+int main() {
+    int a[100], n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    quickSort(a, 0, n - 1);
+
+    printf("Sorted array:\n");
+    for(i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
